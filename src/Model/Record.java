85a29cdd2582a198607f2c34914850ad5cd5591f
LeoF-07 <fortin.leonardo07@gmail.com>
2025-05-07 13:14:14 +0200

    Aggiunta packages e classi

---
+++ src/Model/Record.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class Record{
+
+    public String matricola;
+    public String cognome;
+    public String nome;
+    public String citta;
+    public String indirizzo;
+    public static int L = 100;
+
+    static public String normalizza(String s){           // aggiunge spazi alla stringa, fino a 20  caratteri
+        int i = s.length();
+        for(; i < 20; i++) s = s + " ";
+        return s;
+    }
+
+    public Record(){
+        matricola = normalizza(matricola);
+        cognome = normalizza(cognome);
+        nome = normalizza(nome);
+        citta = normalizza(citta);
+        indirizzo = normalizza(indirizzo);}
+
+    public Record (String matricola, String cognome, String nome, String citta, String indirizzo){
+        this.matricola = normalizza(matricola);
+        this.cognome = normalizza(cognome);
+        this.nome = normalizza(nome);
+        this.citta = normalizza(citta);
+        this.indirizzo = normalizza(indirizzo);
+    }
+
+    public String toString() {
+        return matricola + cognome + nome + citta + indirizzo;
+    }
+
+}
---
+++ src/TesterPatternMVC.java
@@ -0,0 +1,46 @@
+import Model.Record;
+
+public class TesterPatternMVC{
+
+    public static Anagrafico arc;
+
+    public static void main(String[] args){
+        arc = new Anagrafico();
+
+        arc.apriFile();
+        arc.aggiungi("AZ999", "Gialli", "Federica", "San dona del Piave", "Piazza Vittorio II 6");
+        arc.aggiungi("AZ123", "Rossi", "Gianluca", "Castelfranco", "Lungo Sile 6");
+
+        arc.vediTutti();
+
+        int pos = arc.cercaMatricola("AZ123");
+        if (pos > 0) {
+            String s = arc.recPosizione(pos);
+            System.out.println("\nRecord trovato:\n" + s);
+        }
+        else System.out.println("\nMatricola inesistente");
+
+        arc.modifica(200, "Mestre", "Garibaldi 35");
+
+
+        stampaMatricola(200);
+
+
+        arc.reInizializzaFile();
+
+        arc.chiudiFile();
+    }
+
+    public static void stampaMatricola(int posizione){
+        String s = arc.recPosizione(posizione);
+        System.out.println("\nMatricola numero " + (posizione / Record.L + 1) + ":");
+        
+        System.out.print(s.substring(0,20));
+        System.out.print(s.substring(20,40));
+        System.out.print(s.substring(40,60));
+        System.out.print(s.substring(60,80));
+        System.out.print(s.substring(80,100));
+        System.out.println();
+    }
+
+}
